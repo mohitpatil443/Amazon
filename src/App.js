7a220cf8a75aa2cc91071bb9cf441e30c31123a2
mohitpatil443 <mohitpatil443@gmail.com>
2021-09-19 12:06:36 -0400

    deploy

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
+function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    // Only runs once when the component loads
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is : ", authUser);
+
+      if (authUser) {
+        // the user was already logged in or just logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+        // the authUser is null and the user is logged out
+      }
+    }); //Listener Login and Logout
+  }, []);
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
